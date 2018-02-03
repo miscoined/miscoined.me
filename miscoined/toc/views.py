3f1845e0d50cfd811e0f9d53bda4b2c044a783ac
miscoined <miscoined@gmail.com>
2018-02-04 04:53:01 +1100

    Rename character form to character.html

--- miscoined/toc/views.py
+++ miscoined/toc/views.py
@@ -2,13 +2,15 @@ from flask import render_template, request, redirect, url_for
 
 from miscoined import app
 from miscoined.toc.abilities import Abilities
+from miscoined.toc.occupations import Occupations
 
 
 @app.route("/toc")
 @app.route("/toc/")
 def character_create():
     return render_template(
-        'toc/toc.html',
+        'toc/character.html',
+        occupations=Occupations.OCCUPATIONS,
         investigative_abilities=Abilities.investigative,
-        general_abilities=Abilities.general
+        general_abilities=Abilities.general,
     )
