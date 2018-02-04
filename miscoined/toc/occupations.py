0584ac702042f8445bcda6368e329aea850c8ab5
miscoined <miscoined@gmail.com>
2018-02-05 10:28:41 +1100

    Add occupations

---
+++ miscoined/toc/occupations.py
@@ -0,0 +1,13 @@
+"""
+Hold occupations
+"""
+
+class Occupations:
+
+    OCCUPATIONS = {
+        'test': {
+            'credit rating': {'min': 2, 'max': 4},
+            'abilities': ['district knowledges']
+        }
+    }
+
