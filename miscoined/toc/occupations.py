b58f3b4a4eaaf02a95e34643180458c661496cfc
miscoined <miscoined@gmail.com>
2018-02-06 09:33:01 +1100

    Add additional testing characters

--- miscoined/toc/occupations.py
+++ miscoined/toc/occupations.py
@@ -2,12 +2,18 @@
 Hold occupations
 """
 
+
 class Occupations:
 
     OCCUPATIONS = {
-        'test': {
-            'credit rating': {'min': 2, 'max': 4},
-            'abilities': ['district knowledges']
-        }
+        "antiquarian": {
+            "credit rating": {"min": 2, "max": 5},
+            "abilities": ["architecture", "art history", "bargain", "history",
+                          "languages", "law", "library use"],
+        },
+        "author": {
+            "credit rating": {"min": 1, "max": 3},
+            "abilities": ["art", "history", "languages", "library use", "oral history",
+                          "bullshit detector"],
+        },
     }
-
