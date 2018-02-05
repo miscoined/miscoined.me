2473a2abf8fa6257a66ff89a3afb6208ba0145df
miscoined <miscoined@gmail.com>
2018-02-06 10:53:51 +1100

    Add occupation bindings and field template

--- miscoined/toc/occupations.py
+++ miscoined/toc/occupations.py
@@ -5,15 +5,14 @@ Hold occupations
 
 class Occupations:
 
-    OCCUPATIONS = {
-        "antiquarian": {
-            "credit rating": {"min": 2, "max": 5},
-            "abilities": ["architecture", "art history", "bargain", "history",
-                          "languages", "law", "library use"],
-        },
-        "author": {
-            "credit rating": {"min": 1, "max": 3},
-            "abilities": ["art", "history", "languages", "library use", "oral history",
-                          "bullshit detector"],
-        },
-    }
+    OCCUPATIONS = [{
+        "name": "antiquarian",
+        "credit rating": {"min": 2, "max": 5},
+        "abilities": ["architecture", "art history", "bargain", "history",
+                      "languages", "law", "library use"],
+    }, {
+        "name": "author",
+        "credit rating": {"min": 1, "max": 3},
+        "abilities": ["art", "history", "languages", "library use", "oral history",
+                      "bullshit detector"],
+    }]
