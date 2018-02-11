aeb4182dd931a2897608dca0910833e5b726fe72
miscoined <miscoined@gmail.com>
2018-02-11 20:46:30 +1100

    Calculate investigative categories client-side

--- miscoined/toc/views.py
+++ miscoined/toc/views.py
@@ -11,6 +11,7 @@ from miscoined.toc.character import Character
 def landing():
     return redirect(url_for('character_create'))
 
+
 @app.route("/toc/character", methods=["GET", "POST"])
 def character_create():
 
@@ -20,8 +21,5 @@ def character_create():
     return render_template(
         'toc/character.html',
         occupations=data.occupations(),
-        investigative_categories=list(
-            set(i['category'][1] for i in data.abilities()
-                if "investigative" in i["category"])),
         character=Character.new(),
     )
